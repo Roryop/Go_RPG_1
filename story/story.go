5029457c9f271d04a0f6e1deb5246c74caa676db
Roryop <roryop@tumo.world>
2024-12-14 23:05:36 +0100

    added story element

--- story/story.go
+++ story/story.go
@@ -2,8 +2,29 @@ package story
 
 import (
 	"fmt"
+	"time"
 )
 
 func Prologue() {
+	EmptyLine()
 	fmt.Println("The story begins")
+	EmptyLine()
+	LongWait()
+	fmt.Println("Are you ready, Adventurer?")
+	ShortWait()
+	EmptyLine()
+	ShortWait()
+	fmt.Println("What awaits you is a world filled with dangers at every corner.")
+}
+
+func EmptyLine() {
+	fmt.Println("-------------------------------------------------")
+}
+
+func LongWait() {
+	time.Sleep(5 * time.Second)
+}
+
+func ShortWait() {
+	time.Sleep(1 * time.Second)
 }
