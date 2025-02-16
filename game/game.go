5f539a28320b0610e04d0aeffdadd89322730872
Martin_Schmal <schmal.martin17@gmail.com>
2025-02-16 21:13:06 +0100

    Alles das was Ferit im Tutorial hatte soweit ich konnte (eigentlich kann)

--- game/game.go
+++ game/game.go
@@ -14,3 +14,24 @@ func ChooseGameLevel() int {
 	fmt.Println("Du hast Level", game_level, "ausgewählt.")
 	return game_level
 }
+
+func Chooseworld(world_barrier int) string {
+
+	var world string
+	var isInLoop bool
+	for isInLoop {
+		fmt.Println("In welche Welt willst du?") // This example is for User deciding world
+		fmt.Scanln(&world)
+
+		switch {
+		case world == "Cyberpunk" && world_barrier >= 1: // Namen der Welten bitte Eintragen (hab erstmal 3 geschrieben)
+			isInLoop = false
+		case world == "Welt2" && world_barrier >= 2: // Den welten fehlen vielleicht Attribute aber KA wie mann die schreibt und ob die überhaupt hier sein sollen
+			isInLoop = false
+		case world == "Welt3" && world_barrier >= 3:
+			isInLoop = false
+		}
+	}
+	fmt.Println("Du hast", world, "ausgewählt")
+	return world
+}
