59987d2a7e66c65da041143b80194bfa42e4ea36
Roryop <roryop@tumo.world>
2025-03-13 14:15:54 +0100

    added string to event Robbery

--- story/eventsCyberpunk.go
+++ story/eventsCyberpunk.go
@@ -4,12 +4,15 @@ import (
 	"start/game"
 	"start/gear"
 	"start/player"
+	"start/text"
 )
 
 // Gets player, inventory, current stats, world, world_barrier
 // Executes Event Robbery
 // Returns player, inventory, current  stats, world_barrier
 func Robbery(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int, typ int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+
+	text.Print("Du wirst von einem Räuber überfallen!!!")
 
 	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, typ)
 
