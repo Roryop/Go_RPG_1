b2380eac8319dac8257cab91caaa884c85613db8
Martin_Schmal <schmal.martin17@gmail.com>
2025-03-15 13:44:06 +0100

    func Verletzterbewohner

--- story/eventsPostop.go
+++ story/eventsPostop.go
@@ -100,3 +100,33 @@ func Stammarzt(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, at
 }
 
 ////////////////////Funktionen für Geisterstdt///////////////////////////
+
+// Spieler wählt aus ob er steine an den Rotzbuben zurückwirft
+// Gets PlayerStats
+// returns nothing
+func Verletzterbewohner(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int) {
+	var choice int
+
+	text.Print("Du triffst einen verletzten Bewohner.")
+
+	// Auswahl Pilz essen oder nicht
+	text.Print("Willst du ihm helfen?")
+	fmt.Println("1: Ja")
+	fmt.Println("2: Nein")
+	fmt.Scanln(&choice)
+
+	if choice == 1 {
+		text.Print("Du bringst den Bewohner in eine lehre Bude, bindest seine Wunden und gehst.")
+		text.Print("Kurz danach siehst du aus einer Ferne wie diese Bude von jemandem in die Luft gesprengt wird.")
+		text.Print("...")
+		text.Print("Naja... Letzendlich hast du ihm geholfen.")
+		// + Karma
+	} else {
+		text.Print("Du entscheidest dich ihn schmerzhaft sterben zu lassen.")
+	}
+	///////////////////////////////////////////////
+	///////////////////////////////////////////////
+	/////////            KARMA           //////////
+	///////////////////////////////////////////////
+	///////////////////////////////////////////////
+}
