6a6e511f19376e0e7f3119099aa8a114f1616fab
Martin_Schmal <schmal.martin17@gmail.com>
2025-03-15 12:59:49 +0100

    func Mutantenratte

--- story/eventsPostop.go
+++ story/eventsPostop.go
@@ -1,7 +1,24 @@
 package story
 
+import (
+	"start/game"
+	"start/gear"
+	"start/player"
+	"start/text"
+)
+
 /////////////////////Funktionen für Ground Zero///////////////////////////
 
-////////////////Funktionen für Siedlung///////////////////////////
+// Spieler wird von einer menschengroßen Mutantenratte angegriffen
+// Gets Playerstats, Kampfalgoritmus
+// Returns Playerstats nach Kampf
+func Mutantenratte(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+
+	text.Print("Du wirst von einer Menschengroßen Mutantenratte angegriffen!")
+
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+
+	return player1, inventory, hp, att, def, rec, world_barrier
+}
 
 ////////////////////Funktionen für Geisterstdt///////////////////////////
