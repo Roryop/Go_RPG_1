2086e301f9c6096f48f4050ea0a466e4dd1ce2ed
Martin_Schmal <schmal.martin17@gmail.com>
2025-03-15 13:00:22 +0100

    func Dreikoepfigeschlange

--- story/eventsPostop.go
+++ story/eventsPostop.go
@@ -21,4 +21,28 @@ func Mutantenratte(player1 *player.Player, inventory [10]*gear.InventorySlot, hp
 	return player1, inventory, hp, att, def, rec, world_barrier
 }
 
+// Spieler wird von einer dreikoepfigen Schlange angegriffen
+// Gets Playerstats, Kampfalgoritmus
+// Returns Playerstats nach Kampf
+func Dreikoepfigeschlange(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+
+	text.Print("Du wirst von eine dreikoepfigen Schlange angegriffen!")
+
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+
+	return player1, inventory, hp, att, def, rec, world_barrier
+}
+
+// Spieler wird von einer laufenden Makrowelle angegriffen
+// Gets Playerstats, Kampfalgoritmus
+// Returns Playerstats nach Kampf
+func Laufendemakrowelle(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+
+	text.Print("Du wirst von einer laufenden Makrowelle angegriffen!")
+
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+
+	return player1, inventory, hp, att, def, rec, world_barrier
+}
+
 ////////////////////Funktionen für Geisterstdt///////////////////////////
