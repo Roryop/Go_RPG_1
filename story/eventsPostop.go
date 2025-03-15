7d4135ef0ae66664c68a3cf5ed291a94d7370857
Martin_Schmal <schmal.martin17@gmail.com>
2025-03-15 13:08:35 +0100

    func Plünderer + korrigiert func Mutantenratte, func Dreikoepfigeschlange und func Laufendemakrowelle

--- story/eventsPostop.go
+++ story/eventsPostop.go
@@ -1,6 +1,7 @@
 package story
 
 import (
+	"fmt"
 	"start/game"
 	"start/gear"
 	"start/player"
@@ -16,7 +17,7 @@ func Mutantenratte(player1 *player.Player, inventory [10]*gear.InventorySlot, hp
 
 	text.Print("Du wirst von einer Menschengroßen Mutantenratte angegriffen!")
 
-	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 7)
 
 	return player1, inventory, hp, att, def, rec, world_barrier
 }
@@ -28,7 +29,7 @@ func Dreikoepfigeschlange(player1 *player.Player, inventory [10]*gear.InventoryS
 
 	text.Print("Du wirst von eine dreikoepfigen Schlange angegriffen!")
 
-	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 8)
 
 	return player1, inventory, hp, att, def, rec, world_barrier
 }
@@ -40,7 +41,35 @@ func Laufendemakrowelle(player1 *player.Player, inventory [10]*gear.InventorySlo
 
 	text.Print("Du wirst von einer laufenden Makrowelle angegriffen!")
 
-	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 6)
+	player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 9)
+
+	return player1, inventory, hp, att, def, rec, world_barrier
+}
+
+////////////////Funktionen für Siedlung///////////////////////////
+
+// Spieler wird von einer laufenden Makrowelle angegriffen
+// Gets Playerstats, Kampfalgoritmus
+// Returns Playerstats nach Kampf
+func Plünderer(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, world_barrier int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+	var choice int
+
+	text.Print("Die Siedlung wird von Plünderern überrannt.")
+
+	// Auswahl Pilz essen oder nicht
+	text.Print("Willst du bei der Verteidigung helfen?")
+	fmt.Println("1: Ja")
+	fmt.Println("2: Nein")
+	fmt.Scanln(&choice)
+
+	if choice == 1 {
+
+		text.Print("Du wirst von einem Plünderer angegriffen!")
+		player1, inventory, hp, att, def, rec, world_barrier = game.Fight(player1, inventory, hp, att, def, rec, world, world_barrier, 10)
+
+	} else {
+		text.Print("Du bist wie ein Feige weggerannt")
+	}
 
 	return player1, inventory, hp, att, def, rec, world_barrier
 }
