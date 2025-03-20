67e6943bc820e78460a684f2f84331f3ae4e26e4
Martin_Schmal <schmal.martin17@gmail.com>
2025-03-20 18:41:58 +0100

    func Schlaegerei

--- story/eventsWildwest.go
+++ story/eventsWildwest.go
@@ -2,15 +2,16 @@ package story
 
 import (
 	"fmt"
+	"start/gear"
 	"start/player"
 	"start/text"
 )
 
 //////////////////////Funktionen für Bar///////////////////////////
 
-// Spieler wählt aus ob er steine an den Rotzbuben zurückwirft
+// Spieler wählt aus ob er jemanden für Whiskey ausgeben lässt
 // Gets PlayerStats
-// returns nothing
+// returns karma
 func Whiskey(player1 *player.Player) {
 	var choice int
 
@@ -29,6 +30,32 @@ func Whiskey(player1 *player.Player) {
 	} else {
 		text.Print("Aus Höflichkeit oder aus dem Grund, dass der Unbekannte unerträglich stank. Lehnst du ab.")
 	}
+}
+
+// Spieler wählt aus ob er sich in eine Schlaegerei einmischen will
+// Gets Playerstats, Kampfalgoritmus
+// Returns Playerstats nach Kampf
+func Schlaegerei(player1 *player.Player, inventory [10]*gear.InventorySlot, hp, att, def, rec int, world string, player_level int) (*player.Player, [10]*gear.InventorySlot, int, int, int, int, int) {
+	var choice int
+
+	text.Print("In der Bar bricht eine Schlaegerei aus!")
+
+	// Auswahl Verteidigung helfen oder nicht
+	text.Print("Willst du deich einmischen?")
+	fmt.Println("1: Ja")
+	fmt.Println("2: Nein")
+	fmt.Scanln(&choice)
+
+	if choice == 1 {
+		text.Print("Es kommt zu einem Kampf eins gegen eins!")
+		player1, inventory, hp, att, def, rec, player_level = Fight(player1, inventory, hp, att, def, rec, world, player_level, 14)
+
+	} else {
+		text.Print("Du hast dich unter dem Bartisch versteckt. Bis alles vorbei ging.")
+
+	}
+
+	return player1, inventory, hp, att, def, rec, player_level
 }
 
 ////////////////Funktionen für Shertrif Büro///////////////////////////
